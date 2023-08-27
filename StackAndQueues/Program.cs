443999dc56c675c25baaf184fffb1b97485fbb24
Petar Atanasov <petaratanasov.gh@gmail.com>
2023-08-27 15:59:59 +0300

    Add project files.

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] input = Console.ReadLine().Split();
+
+Stack<string> exppresions = new Stack<string>(input.Reverse());
+
+int result = int.Parse(exppresions.Pop());
+
+while (exppresions.Any())
+{
+    string sign = exppresions.Pop();
+    int currentNumber = int.Parse(exppresions.Pop());
+
+    if (sign == "+")
+    {
+        result += currentNumber;
+
+    }
+    else if (sign == "-")
+    {
+        result -= currentNumber;
+
+    }
+}
+Console.WriteLine(result);
