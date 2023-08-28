b0dc6b325cd82bd84d1ff17f584e93408185fec7
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-08-28 14:14:17 +0300

    Supermarket - Stack Exercise

---
+++ Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = Console.ReadLine();
+
+Queue<string> names = new Queue<string>();
+
+while (input != "End")
+{
+    if (input == "Paid")
+    {
+        foreach (var item in names)
+        {
+            Console.WriteLine(item);
+        }
+        names.Clear();
+    }
+    else
+    {
+        names.Enqueue(input);
+    }
+
+    input = Console.ReadLine();
+}
+if (names.Any())
+{
+    Console.WriteLine($"{names.Count} people remaining.");
+}
+else
+{
+    Console.WriteLine("0 people remaining.");
+}
+
