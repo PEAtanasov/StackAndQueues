e90185f7b50cd385cbdf4646da15b6bd95f53ff9
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-08-29 23:05:09 +0300

    HotPotato-Queues exercise

---
+++ HotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] inNames = Console.ReadLine().Split();
+
+int n = int.Parse(Console.ReadLine());
+
+Queue<string> namesPlayers = new Queue<string>(inNames);
+
+int counter = 0;
+
+while (namesPlayers.Count > 1)
+{
+    counter++;
+    if (counter == n)
+    {
+        string removedPlayer = namesPlayers.Dequeue();
+        //namesPlayers.Dequeue();
+        Console.WriteLine($"Removed {removedPlayer}");
+        counter = 0;
+        continue;
+    }
+    string nameToRotate = namesPlayers.Dequeue();
+    namesPlayers.Enqueue(nameToRotate);
+    //Console.WriteLine(counter);
+}
+//string winner = namesPlayers.Dequeue();
+Console.WriteLine($"Last is {namesPlayers.Dequeue()}");
+
+
