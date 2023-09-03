2dc9785961753d13ff2270fd1c112e5f66cf8d98
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-03 14:35:15 +0300

    FastFood Queue Exercise

---
+++ FastFood/Program.cs
@@ -0,0 +1,32 @@
+﻿int quantityOfFood = int.Parse(Console.ReadLine());
+
+int[] ordersInfo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> orders = new Queue<int>(ordersInfo);
+
+Console.WriteLine(orders.Max());
+
+while (orders.Any())
+{
+    if (quantityOfFood - orders.Peek() >= 0)
+    {
+        quantityOfFood -= orders.Dequeue();
+    }
+    else
+    {
+        break;
+    }
+}
+if (orders.Any())
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
+
+
