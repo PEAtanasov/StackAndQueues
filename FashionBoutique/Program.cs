91b9bcb6a277ef283b4e1b291b07c5fba2383b7a
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-03 14:42:24 +0300

    Stack exercise

---
+++ FashionBoutique/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] clothesQuantity = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> boxesOfClothes = new Stack<int>(clothesQuantity);
+
+int rackCapacity = int.Parse(Console.ReadLine());
+
+int rackToWork = rackCapacity;
+int rackCounts = 1;
+
+while (boxesOfClothes.Any())
+{
+    if (rackToWork - boxesOfClothes.Peek() >= 0)
+    {
+        rackToWork -= boxesOfClothes.Pop();
+    }
+    else
+    {
+        rackCounts++;
+        rackToWork = rackCapacity;
+    }
+}
+Console.WriteLine(rackCounts);
