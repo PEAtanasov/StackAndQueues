ce8840ecc206ab6142995b360c5d77a80fa5a5cf
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-03 14:32:07 +0300

    Maximum and minimum number stack stack collection exercise

---
+++ MaximumAndMinimumNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+    string command = Console.ReadLine();
+    int[] commandInfo = command
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int commanToDo = commandInfo[0];
+    int numberToPush = 0;
+
+    if (commandInfo.Length > 1)
+    {
+        numberToPush = commandInfo[1];
+    }
+
+    if (commanToDo == 1)
+    {
+        stack.Push(numberToPush);
+    }
+    else if (commanToDo == 2)
+    {
+        stack.Pop();
+    }
+    else if (commanToDo == 3)
+    {
+        if (stack.Any())
+        {
+            Console.WriteLine(stack.Max());
+        }
+    }
+    else if (commanToDo == 4)
+    {
+        if (stack.Any())
+        {
+            Console.WriteLine(stack.Min());
+        }
+    }
+}
+Console.WriteLine(string.Join(", ", stack));
