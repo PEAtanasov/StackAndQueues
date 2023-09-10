b722f0853f185d0b92aa7dbdea533bf539f9457e
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-10 13:38:26 +0300

    SongsQueue Exercise Queue Collection

---
+++ SongsQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] songs = Console.ReadLine().Split(", ");
+
+Queue<string> playList = new Queue<string>(songs);
+
+string command = Console.ReadLine();
+
+while (playList.Any())
+{
+    if (command.Contains("Add"))
+    {
+        string songToAdd = command.Substring(4, command.Length - 4);
+
+        //Console.WriteLine(songToAdd);
+
+        if (!playList.Contains(songToAdd))
+        {
+            playList.Enqueue(songToAdd);
+        }
+        else
+        {
+            Console.WriteLine($"{songToAdd} is already contained!");
+        }
+    }
+    else if (command.Contains("Play"))
+    {
+        playList.Dequeue();
+    }
+    else if (command.Contains("Show"))
+    {
+        Console.WriteLine(string.Join(", ", playList));
+    }
+
+
+
+    command = Console.ReadLine();
+}
+Console.WriteLine("No more songs!");
